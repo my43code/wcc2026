a71f65e3359754b488930fdfaeb51d1ab579f964
Manu Maso <masomanu23@gmail.com>
2026-08-02 11:11:30 +1000

    up

--- backend/app/main.py
+++ backend/app/main.py
@@ -55,7 +55,7 @@ Category = Literal["news_events", "early_learning", "primary_school", "secondary
 app = FastAPI(title="Waigani Christian College API", version="2.0.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "https://qemfloodlswcorxwqrzw.supabase.co"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
